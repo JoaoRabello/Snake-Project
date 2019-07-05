a7986757dca517fd0d6cf7ee50382e72150f4310
João Vitor Rabello de Araujo <rabellois@hotmail.com>
2019-07-05 18:59:44 -0300

    Publishing Repository
    -GameManager created (generates the board)
    -PlayerBehaviour created (the player moves constantly to a direction)

---
+++ SnakeClone/Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject whiteGround;
+    public GameObject blackGround;
+
+    [HideInInspector]
+    public List<Vector2> ground;
+    public int linhas = 2;
+    public int colunas = 2;
+
+    void Awake()
+    {
+        CreateWorld();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void CreateWorld()
+    {
+        bool white = true;
+        Vector2 pos = transform.position;
+        for (int x = 0; x < linhas; x++)
+        {
+            pos.x = transform.position.x;
+            for (int y = 0; y < colunas; y++)
+            {
+                ground.Add(pos);
+
+                if (white)
+                {
+                    Instantiate(whiteGround, pos, Quaternion.identity);
+                    white = false;
+                }
+                else
+                {
+                    Instantiate(blackGround, pos, Quaternion.identity);
+                    white = true;
+                }
+                pos.x += 1;
+            }
+            pos.y -= 1;
+        }
+    }
+}
---
+++ SnakeClone/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    protected enum Direction { RIGHT, LEFT, UP, DOWN };
+    protected Direction op;
+
+    private Vector2 desiredPosition;
+    public float moveFrequency;
+    private float counter;
+    void Start()
+    {
+        desiredPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if(counter >= moveFrequency)
+        {
+            counter = 0;
+
+            switch (op)
+            {
+                case Direction.UP:
+                    desiredPosition += Vector2.up;
+                    break;
+                case Direction.DOWN:
+                    desiredPosition += Vector2.down;
+                    break;
+                case Direction.LEFT:
+                    desiredPosition += Vector2.left;
+                    break;
+                case Direction.RIGHT:
+                    desiredPosition += Vector2.right;
+                    break;
+            }
+
+            MoveTo(desiredPosition);
+        }
+        else
+        {
+            counter += Time.deltaTime;
+        }
+    }
+
+    void MoveTo(Vector2 desiredPosition)
+    {
+        transform.position = desiredPosition;
+    }
+}
