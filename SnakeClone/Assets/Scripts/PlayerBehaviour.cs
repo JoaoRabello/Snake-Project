49ea7c973b6a7548f73fb72d2bb6676a409e24c2
João Vitor Rabello de Araujo <rabellois@hotmail.com>
2019-07-05 23:11:00 -0300

    Body Follow
    -Body is now following the head
    -Started the fruit mech

--- SnakeClone/Assets/Scripts/PlayerBehaviour.cs
+++ SnakeClone/Assets/Scripts/PlayerBehaviour.cs
@@ -11,9 +11,15 @@ public class PlayerBehaviour : MonoBehaviour
     public float moveFrequency;
     private float counter;
 
+    public List<GameObject> body;
+    int bodySize = 2;
+    public List<Vector2> path;
+
     protected virtual void Start()
     {
         desiredPosition = transform.position;
+        path.Add(body[1].transform.position);
+        path.Add(transform.position);
     }
 
     protected void SwitchDirection()
@@ -37,7 +43,7 @@ public class PlayerBehaviour : MonoBehaviour
                     desiredPosition += Vector2.right;
                     break;
                 default:
-                    desiredPosition += Vector2.right;
+                    desiredPosition += Vector2.up;
                     break;
             }
             MoveTo(desiredPosition);
@@ -51,5 +57,31 @@ public class PlayerBehaviour : MonoBehaviour
     void MoveTo(Vector2 desiredPosition)
     {
         transform.position = desiredPosition;
+        path.Add(transform.position);
+        MoveBody();
+        path.Remove(path[0]);
+    }
+
+    private void MoveBody()
+    {
+        for(int i = 1; i < body.Count; i++)
+        {
+            body[i].transform.position = path[i];
+        }
+    }
+
+    protected void GrowBody()
+    {
+        GameObject newBody = Instantiate(body[1], path[1], Quaternion.identity);
+        body.Add(newBody);
+    }
+
+    private void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.CompareTag("Fruit"))
+        {
+            GrowBody();
+            Destroy(c.gameObject);
+        }
     }
 }
